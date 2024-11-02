e777ff18cad04956d0ae528f8305ec75e213f4f1
Ayomide Ajayi <lancerajayi@yahoo.com>
2024-11-02 15:06:48 +0000

    New S3 Bucket

--- Backend.tf
+++ Backend.tf
@@ -3,7 +3,7 @@
 terraform {
 
   backend "s3" {
-    bucket = "pipeline-cicd-store"
+    bucket = "cicd-pipeline-store"
     key    = "env/cicd/terraform.tfstate"
     region = "eu-west-2"
     encrypt = false
